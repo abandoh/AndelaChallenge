5f163a8d39d878aa692a155d62f4c6639c458b80
abandoh <abandoh@vvu.edu.gh>
2019-07-17 11:46:55 +0000

    Correcting the SSL Error

--- app/src/main/java/com/dambana/andelachallenge/AboutActivity.java
+++ app/src/main/java/com/dambana/andelachallenge/AboutActivity.java
@@ -2,6 +2,9 @@ package com.dambana.andelachallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.View;
@@ -16,17 +19,45 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.setTitle("About ALC!!!");
         setContentView(R.layout.activity_about);
-
         WebView web = (WebView) findViewById(R.id.webViewAndela);
-        web.setWebViewClient(new WebViewClient());
+        //web.setWebViewClient(new WebViewClient());
+
+        web.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+                // for SSLErrorHandler
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(AboutActivity.this);
+                builder.setMessage(R.string.notification_error_ssl_cert_invalid);
+                builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        handler.proceed();
+                    }
+                });
+                builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        handler.cancel();
+                    }
+                });
+                final AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
         web.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-        //web.loadUrl("https://www.andela.com/alc");
+        web.loadUrl("https://www.andela.com/alc");
+        /*
         web.setWebViewClient(new WebViewClient() {
             @Override
             public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
                 handler.proceed();
             }
         });
-        web.loadUrl("http://www.andela.com/alc");
+        */
+
+
+        //web.loadUrl("http://www.andela.com/alc");
     }
 }
